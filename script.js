f0c6ea7062c17a45b60142997fa90e1a3ddc099d
Amy Do <amy@Amys-MBP.attlocal.net>
2021-06-09 20:52:55 -0500

    modified index and script files

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// TODO: add code here
+window.addEventListener('load', function() {
+    
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      console.log(json);
+      const container = document.querySelector('#container');
+      let astronauts = '';
+      for (astronaut of json) {
+        astronauts += `
+        <div class="astronaut">
+          <div class="bio">
+            <h3>${astronaut.firstName} </h3>
+          </div>
+        </div>
+        `;
+      }
+      container.innerHTML = astronauts;
+    });
+  
+  });
