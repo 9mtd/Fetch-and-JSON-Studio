63e694dc3d83a5c0736655fbda79110a1d3bc9a8
Amy Do <amy@Amys-MBP.attlocal.net>
2021-06-09 21:14:20 -0500

    added all astronauts info

--- script.js
+++ script.js
@@ -8,14 +8,24 @@ window.addEventListener('load', function() {
       let astronauts = '';
       for (astronaut of json) {
         astronauts += `
+
         <div class="astronaut">
           <div class="bio">
-            <h3>${astronaut.firstName} </h3>
-          </div>
+            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+        <ul>
+            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+            <li>Active: ${astronaut.active}</li>
+            <li>Skills: ${astronaut.skills}</li>
+        </ul>
+            </div>
+            <img class="avatar" src="${astronaut.picture}">
         </div>
+
         `;
       }
       container.innerHTML = astronauts;
     });
   
+
+
   });
